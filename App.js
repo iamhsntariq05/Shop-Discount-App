9ebc63423048b7f75a4b95018c5d95b8dea9748a
unknown <Hassantariq5544@gmail.com>
2020-11-28 18:45:41 +0500

    Discount app

--- App.js
+++ App.js
@@ -1,21 +1,283 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  Modal,
+  FlatList,
 
-export default function App() {
+} from 'react-native';
+
+
+const App = () => {
+
+
+
+  const [originalPrice, setOriginalPrice] = useState("");
+  const [discountPrc, setDicountPrc] = useState("");
+  const [savedAmount, setSavedAmount] = useState("0.00");
+  const [finalPrice, setFinalPrice] = useState("0.00");
+
+  const [calError, setCalError] = useState("");
+
+  const [history, setHistory] = useState([""]);
+  const [modalVisible, setModalVisible] = useState(false);
+
+
+  calculateDiscount = () => {
+    if (discountPrc <= 100 && discountPrc >= 0 && originalPrice >= 0) {
+      var saved = (originalPrice * discountPrc) / 100;
+      var final_Price = originalPrice - saved;
+      setSavedAmount(saved.toFixed(2));
+      setFinalPrice(final_Price.toFixed(2));
+      setCalError("")
+    } else if (discountPrc > 100) {
+      setCalError("Discount Cannot be greater than 100%");
+    } else if (originalPrice < 0 || discountPrc < 0) {
+      setCalError("Original Price or Discount Price can't be 0");
+    }
+  }
+
+  saveResult = () => {
+    var space = " -- ";
+    var result = "Rs: " + originalPrice + space + discountPrc + space + "Rs: " + finalPrice;
+    console.log(result);
+    setHistory(oldHistory => [...history, result]);
+
+    setOriginalPrice("");
+    setDicountPrc("");
+  }
+
+  viewHistory = () => {
+    setModalVisible(true);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.headerTexts}> CART </Text>
+        </View>
+      </View> 
+
+
+
+      <View style={styles.mainView}>
+        <TextInput keyboardType={"number-pad"} value={originalPrice} onChangeText={(orgPrice) => setOriginalPrice(orgPrice)} style={styles.textFields} placeholder={"Actual Price"} />
+        <View style={{ paddingTop: 10 }} />
+        <TextInput keyboardType={"number-pad"} value={discountPrc} onChangeText={(discountPercentage) => setDicountPrc(discountPercentage)} style={styles.textFields} placeholder={"Discount"} />
+        <View style={{ paddingTop: 20 }} />
+        <TouchableOpacity onPress={() => calculateDiscount()} style={styles.calcBtn}>
+          <Text style={styles.calcBtnText}>Calculate</Text>
+        </TouchableOpacity>
+        <View style={{ paddingTop: 10 }} />
+        <Text style={{ fontSize: 15, color: 'red' }}>{calError}</Text>
+        <View style={{ paddingTop: 10 }} />
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={styles.Fprice}>Discount Price </Text>
+         
+          <Text style={styles.Fprice}>Saved</Text>
+
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={styles.finalPriceText}>Rs {finalPrice}</Text>
+          <Text style={[styles.finalPriceText, { color: 'red' }]}> Rs {savedAmount}</Text>
+        </View>
+        <View style={{ paddingTop: 15 }} />
+        <TouchableOpacity onPress={() => saveResult()} style={styles.saveBtn}>
+          <Text style={styles.saveBtnText}>Save Calculation</Text>
+        </TouchableOpacity>
+        <View style={{ paddingTop: 10 }} />
+        <TouchableOpacity onPress={() => viewHistory()} style={styles.historyBtn}>
+          <Text style={styles.historyBtnText}>View History</Text>
+        </TouchableOpacity>
+
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            window.alert("Modal has been closed.");
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalHeading}>Discount History</Text>
+              <Text style={styles.firstIndexHistoryText}>Actual Price ~ Discount % ~ Discount Price</Text>
+              <FlatList
+
+                data={history}
+                renderItem={({ item }) => { return <Text style={styles.listTextItem}>{item}</Text> }}
+                keyExtractor={(index) => { return index }} />
+
+              <TouchableOpacity
+                style={{ ...styles.closeHistory, backgroundColor: "#2196F3" }}
+                onPress={() => {
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
+
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  
+  header: {
+    width: 230,
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+
+  },
+  headerTexts: {
+    fontWeight: "bold",
+    fontSize: 30,
+    fontStyle: "italic",
+    letterSpacing: 4,
+    marginTop: 200,
+    paddingLeft : 110,
+  },
+  
+  
+  
+  centeredView: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingTop: 35,
+    paddingBottom: 35,
+    paddingLeft: 10,
+    paddingRight: 10,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  closeHistory: {
+    backgroundColor: "#F194FF",
+    borderRadius: 5,
+    width: 100,
+    height: 30,
+    elevation: 2,
+    justifyContent: 'center'
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  },
+  modalHeading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingBottom: 10
+  },
+  firstIndexHistoryText: {
+    fontSize: 18,
+  },
+
+  headerText: {
+    fontSize: 24,
+    color: 'white'
+  },
+  mainView: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  textFields: {
+    width: "80%",
+    borderColor: "black",
+    borderTopColor: "orange",
+    borderWidth: 4,
+    borderRadius: 20,
+    fontSize: 20,
+    color: "blue",
+    padding: 20,
+    marginBottom: 10,
+  },
+  calcBtn: {
+    height: 40,
+    width: 120,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    elevation: 2,
+  },
+  calcBtnText: {
+    fontSize: 18,
+    color: "black",
+    borderRadius: 80,
+    backgroundColor: "orange",
+
+  },
+  Fprice: {
+    fontSize: 18,
+    justifyContent: 'space-between',
+    paddingHorizontal: 34,
+  },
+  finalPriceText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    justifyContent: 'center',
+    paddingHorizontal: 51,
+
+  },
+  saveBtn: {
+    height: 35,
+    width: 150,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    elevation: 2,
+  },
+  saveBtnText: {
+    fontSize: 18,
+    color: 'lightgreen',
+    fontWeight: "bold",
+  },
+
+  historyBtn: {
     alignItems: 'center',
     justifyContent: 'center',
   },
+  historyBtnText: {
+    fontSize: 18,
+    color: '#566573'
+  },
+  listTextItem: {
+    fontSize: 18
+  },
+
 });
+
+
+export default App;
---
+++ README.md
@@ -0,0 +1,44 @@
+# Shop Discount App
+
+#### Shop Discount App is develope using *React native*, which help the customers/buyers to calculate the discount and save the record.
+
+<img alt="GIF" src="img/3dAssets/aftercal.jpeg" width="340" height="300" />
+<img src="img/3dAssets/beforecalculation.jpeg" width="340" height="300" />
+<br>
+<br>
+<br><hr>
+<br>
+
+### Tools & Technology used:
+<img align="left" alt="iamhsntariq | JS" width=24px src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/javascript.svg">
+<img align="left" alt="iamhsntariq | VS Code" width=24px src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/visualstudio.svg">
+<img align="left" alt="iamhsntariq | VS Code" width=24px src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/react.svg">
+
+<br><br>
+
+<details>
+  <summary>:zap: Most Used Languages</summary>
+
+  ![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=iamhsntariq05)
+
+</details>
+
+<br>
+<hr>
+
+## Author
+### Muhammad Hasan 
+  
+[![LinkedIn Link](https://img.shields.io/badge/Connect-Hsn-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect
+)](https://www.linkedin.com/in/hassan-tariq-a290441b3/)
+
+
+#### Follow my github to stay update
+
+[![GitHub Follow](https://img.shields.io/badge/Connect-Hsn-blue.svg?logo=Github&longCache=true&style=social&label=Follow
+)](https://github.com/iamhsntariq05)
+
+
+If you liked the repo then kindly support it by giving it a star⭐!
+
+Copyright (c) 2020 **Muhammad Hasan**
